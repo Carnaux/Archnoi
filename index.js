e90b8a47685d1a7a96219de1ffaa64d4dc192792
Daniel Fernandes Gonçalves de Oliveira <danielfeenandes@gmail.com>
2019-10-24 01:08:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,18 +5,9 @@ var minX = 0;
 var minY = 0;
 var floorHeight = 15;
 
-var points = [];
-var neighbours = [];
-var mainPoints = [];
-
 var bbox = {xl: minX, xr: maxX, yt: minY, yb: maxY};
-var shapePoints = [];
-var idArr = [];
-var bPos = new THREE.Vector3();
 
 var floors = [];
-
-var diagram;
 
 var floorNumber = 1;
 
@@ -36,7 +27,7 @@ camera.position.y = 50;
 
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-floors(floorNumber)
+floorGeneration(floorNumber)
 
 // drawLines();
 
@@ -49,8 +40,40 @@ var animate = function () {
 
 animate();
 
+function floorGeneration(n){
+    for(let nF = 0; nF < n; nF++){
+    
+        let pointArr = generatePoints(50);
+
+        let diagram = voronoi.compute(pointArr, bbox);
+        
+        let randCell = getRandomCell(pointArr, diagram);
+        let shapePoints = processNeighbours(randCell.neighbours, diagram, 0);
+        removeMainPoints(shapePoints, randCell);
+        removeDuplicates(shapePoints);
+        let M = findMiddle(shapePoints);
+        let ordered = orderByPolar(shapePoints, M);
+        let shape = createShapeByOrder(ordered);
+        
+        // var extrudeSettings = { depth: floorHeight, bevelEnabled: false, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+        // var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+
+        var geometry = new THREE.ShapeGeometry( shape );
+        // var edges = new THREE.EdgesGeometry( geometry );
+        var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color: new THREE.Color("rgb(205,192,176)"), side: THREE.DoubleSide}) );
+       
+        // var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+        // scene.add( line );
+
+    
+        mesh.position.copy(randCell.bPos);
+        floors.push(mesh);
+        scene.add(mesh);
+    }
+}
 
 function generatePoints(n){
+    let points = [];
     for(let i = 0; i < n; i++){
         let pX = parseInt(Math.random() * maxX);
         let pY = parseInt(Math.random() * maxY);
@@ -58,50 +81,25 @@ function generatePoints(n){
         let obj = {
             x: pX,
             y: pY,
-            
         }
 
         points.push(obj)
-    }   
+    }  
+    return points; 
 }
 
-function drawLines(){
-    for(let i = 0; i < diagram.edges.length; i++){
-        var material = new THREE.LineBasicMaterial({
-            color: 0x0000ff
-        });
-        
-        var geometry = new THREE.Geometry();
-        geometry.vertices.push(
-            new THREE.Vector3( diagram.edges[i].va.x,  diagram.edges[i].va.y, 0 ),
-            new THREE.Vector3( diagram.edges[i].vb.x,  diagram.edges[i].vb.y, 0 )
-        );
-        
-        var line = new THREE.Line( geometry, material );
-        scene.add( line );
-    }
-}
-
-function getRandomCell(n){
+function getRandomCell(points, diagram){
     let randomIndex = parseInt(Math.random() * points.length + 1);
     
     let voroId = points[randomIndex].voronoiId;
 
     let cell = diagram.cells[voroId];
 
-    getMainPoints(cell);
+    let mainPoints = getMainPoints(cell);
 
-    bPos.copy(new THREE.Vector3( cell.site.x * -1, cell.site.y * -1 , (floorHeight/2)  * -1));
+    let pos = new THREE.Vector3( cell.site.x * -1, cell.site.y * -1 , (floorHeight/2)  * -1);
     
-    // var dotGeometry = new THREE.Geometry();
-    // dotGeometry.vertices.push(new THREE.Vector3(cell.site.x, cell.site.y,0));
-    // var dotMaterial = new THREE.PointsMaterial({
-    // size: 10,
-    // sizeAttenuation: false,
-    // color: new THREE.Color("rgb(255,0,0)")
-    // });
-    // var dot = new THREE.Points(dotGeometry, dotMaterial);
-    // scene.add(dot);
+    let neighbours = [];
 
     for(let i = 0; i < cell.halfedges.length; i++){
         let current = cell.halfedges[i].edge;
@@ -118,11 +116,21 @@ function getRandomCell(n){
             }
         }
     }
-    // neighbours.push(voroId);
+
+    let obj = {
+        id: voroId,
+        cell: cell,
+        bPos: pos,
+        mainPoints: mainPoints,
+        neighbours: neighbours
+    };
     
+    return obj;
 }
 
 function getMainPoints(cell){
+    let mainPoints = [];
+    let idArr = [];
     for(let i = 0; i < cell.halfedges.length; i++){
 
         let va = new THREE.Vector3( cell.halfedges[i].edge.va.x,  cell.halfedges[i].edge.va.y, 0 );
@@ -146,66 +154,16 @@ function getMainPoints(cell){
        idArr.splice(idArr[i], 1, -1 );
     }
 
+    let obj = {
+        points: mainPoints,
+        idArr: idArr
+    }
+
+    return obj;
 }
 
-function removeMainPoints(){
-    let newArr = [];
-    let tempIdArr = [];
-
-    for(let i = 0; i < idArr.length; i++){
-        let id = idArr[i];
-        if( id != -1){
-            for(let j = 0; j < shapePoints.length; j++){
-                if(mainPoints[id].equals(shapePoints[j])){
-                    tempIdArr.push(j);
-                } 
-            }
-        }
-    }
-
-    for(let i = 0; i < tempIdArr.length; i++){
-        shapePoints.splice(tempIdArr[i], 1, -1 );
-    }
-
-    for(let i = 0; i < shapePoints.length; i++){
-        if(shapePoints[i] == -1){
-            shapePoints.splice(i, 1);
-            i--;
-        }
-    }
-
-    console.log("temp",tempIdArr);
-    console.log("shape", shapePoints);
-    // shapePoints = newArr;
-}
-
-function removeDuplicates(){
-    let newArr = [];
-
-    for(let i = 0; i < shapePoints.length; i++){
-        let p = shapePoints[i];
-        for(let k = 0; k < shapePoints.length; k++){
-            let p2 = shapePoints[k];
-            if(i != k){
-                if(p.equals(p2)){
-                    shapePoints.splice(i, 1, -1);
-                }
-            }
-            
-        }
-    }
-
-    
-    for(let i = 0; i < shapePoints.length; i++){
-        if(shapePoints[i] == -1){
-            shapePoints.splice(i, 1);
-            i--;
-        }
-    }
-    // shapePoints = newArr;
-}
-
-function drawNeighbours(arr, n){
+function processNeighbours(arr, diagram, n){
+    let shapePoints = [];
     for(let i = 0; i < arr.length; i++){
 
         let cell = diagram.cells[arr[i]];
@@ -235,6 +193,63 @@ function drawNeighbours(arr, n){
             
         }
     }
+    return shapePoints;
+}
+
+function removeMainPoints(arr, cell){
+    let idArr = cell.mainPoints.idArr;
+    let mainPoints = cell.mainPoints.points;
+    let tempIdArr = [];
+
+    for(let i = 0; i < idArr.length; i++){
+        let id = idArr[i];
+        if( id != -1){
+            for(let j = 0; j < arr.length; j++){
+                if(mainPoints[id].equals(arr[j])){
+                    tempIdArr.push(j);
+                } 
+            }
+        }
+    }
+
+    for(let i = 0; i < tempIdArr.length; i++){
+        arr.splice(tempIdArr[i], 1, -1 );
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == -1){
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+
+    // console.log("temp",tempIdArr);
+    // console.log("shape", arr);
+    
+}
+
+function removeDuplicates(arr){
+    for(let i = 0; i < arr.length; i++){
+        let p = arr[i];
+        for(let k = 0; k < arr.length; k++){
+            let p2 = arr[k];
+            if(i != k){
+                if(p.equals(p2)){
+                    arr.splice(i, 1, -1);
+                }
+            }
+            
+        }
+    }
+
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == -1){
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    // shapePoints = newArr;
 }
 
 function findMiddle(arr){
@@ -282,35 +297,53 @@ function createShapeByOrder(arr){
     return path;
 }
 
+function drawLines(){
+    for(let i = 0; i < diagram.edges.length; i++){
+        var material = new THREE.LineBasicMaterial({
+            color: 0x0000ff
+        });
+        
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(
+            new THREE.Vector3( diagram.edges[i].va.x,  diagram.edges[i].va.y, 0 ),
+            new THREE.Vector3( diagram.edges[i].vb.x,  diagram.edges[i].vb.y, 0 )
+        );
+        
+        var line = new THREE.Line( geometry, material );
+        scene.add( line );
+    }
+}
+
 function getOutlines(){
     for(let i = 0; i < floors.length; i++){
         
     }
 }
 
-function floors(n){
-    for(let nF = 0; nF < n; nF++){
-       
+function drawShapePoints(geo){
+    let obj = geo.parameters.shapes.curves;
+    let size = obj.length;
+    
+    for(let i = 0; i < size; i++){
+        var dotGeometry = new THREE.Geometry();
+        dotGeometry.vertices.push( new THREE.Vector3(obj[i].v1.x, obj[i].v1.y, 2));
+        var dotMaterial = new THREE.PointsMaterial({
+        size: 10,
+        sizeAttenuation: false,
+        color: new THREE.Color("rgb(255,0,0)")
+        });
+        var dot = new THREE.Points(dotGeometry, dotMaterial);
+        scene.add(dot);
 
-        generatePoints(50);
-
-        diagram = voronoi.compute(points, bbox);
-        
-        getRandomCell(n);
-        drawNeighbours(neighbours, 0);
-        removeMainPoints();
-        removeDuplicates();
-        let M = findMiddle(shapePoints);
-        let ordered = orderByPolar(shapePoints, M);
-        let shape = createShapeByOrder(ordered);
-        
-        var extrudeSettings = { depth: floorHeight, bevelEnabled: false, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-        var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-        var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color: new THREE.Color("rgb(205,192,176)")}) );
-        
-        mesh.position.copy(bPos);
-        floors.push(mesh);
-        scene.add(mesh);
+        var dotGeometry2 = new THREE.Geometry();
+        dotGeometry2.vertices.push(new THREE.Vector3(obj[i].v2.x, obj[i].v2.y, 2));
+        var dotMaterial2 = new THREE.PointsMaterial({
+        size: 10,
+        sizeAttenuation: false,
+        color: new THREE.Color("rgb(255,0,0)")
+        });
+        var dot2 = new THREE.Points(dotGeometry2, dotMaterial2);
+        scene.add(dot2);
     }
+   
 }
-
